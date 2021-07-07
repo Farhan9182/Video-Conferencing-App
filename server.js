d8a0dc7bf12e794bac8a527725ea9936d9728f4c
Farhan9182 <farhaankhaan930@gmail.com>
2021-07-08 00:04:39 +0530

    created HTML template

--- server.js
+++ server.js
@@ -7,4 +7,12 @@ const io = require("socket.io")(server);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.get("/", (req, rsp) => {
+    rsp.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("room", { roomId: req.params.room });
+});
+
 server.listen(process.env.PORT || 3000);
